94af5e7081af25a392e4bb795e95b0a993089adb
Yurii Tovmach <portfolio_yurii_tovmach@ukr.net>
2019-06-18 09:29:31 +0300

    second changed js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputElement = document.getElementById('input')
 const ulElement = document.getElementById('list')
 
 todoList = []
+//upgradeView()
 
 
 selectAllButton.addEventListener('click',() =>{
@@ -11,76 +12,51 @@ selectAllButton.addEventListener('click',() =>{
 
 inputElement.addEventListener('keydown', event =>{
     if(event.key==='Enter' || event.keyCode===13){
-    todoList.push(inputElement.value)
+    todoList.unshift(inputElement.value)
+//    todoList.push(inputElement.value)
     inputElement.value = ''
+
+    upgradeView()
     }
 
 })
 
-const liElement = document.createElement('li')
-liElement.className = "list-group-item"
-ulElement.append(liElement)
+function upgradeView() {
+    ulElement.innerHTML = ''
+    for (const todoItem of todoList){
+        const liElement = document.createElement('li')
+        liElement.className = "list-group-item"
+        ulElement.append(liElement)
 
-const divElement = document.createElement('div')
-divElement.className = 'form-group form-check'
-liElement.append(divElement)
+        const divElement = document.createElement('div')
+        divElement.className = 'form-group form-check'
+        liElement.append(divElement)
 
-const checkboxElement = document.createElement('input')
-divElement.append(checkboxElement)
-checkboxElement.type = 'checkbox'
-checkboxElement.className = 'form-check-input'
+        const checkboxElement = document.createElement('input')
+        divElement.append(checkboxElement)
+        checkboxElement.type = 'checkbox'
+        checkboxElement.className = 'form-check-input'
 
-const labelElement = document.createElement('label')
-divElement.append(labelElement)
-labelElement.className = 'form-check-label'
-labelElement.setAttribute = ('for', 'exampleCheck1')
-labelElement.innerText = 'Check me out'
+        const labelElement = document.createElement('label')
+        divElement.append(labelElement)
+        labelElement.className = 'form-check-label'
+        labelElement.setAttribute = ('for', 'exampleCheck1')
+        labelElement.innerText = todoItem
 
-const buttonDoneElement = document.createElement('button')
-divElement.append(buttonDoneElement)
-buttonDoneElement.type = 'button'
-buttonDoneElement.className = 'btn btn-outline-primary'
-buttonDoneElement.innerText = 'Done'
+        const buttonDoneElement = document.createElement('button')
+        divElement.append(buttonDoneElement)
+        buttonDoneElement.type = 'button'
+        buttonDoneElement.className = 'btn btn-outline-primary'
+        buttonDoneElement.innerText = 'Done'
 
-const buttonRemoveElement = document.createElement('button')
-divElement.append(buttonRemoveElement)
-buttonRemoveElement.type = 'button'
-buttonRemoveElement.className = 'btn btn-outline-primary'
-buttonRemoveElement.innerText = 'Remove'
-
-
-
-
-
-
-//
-//      <li class="list-group-item">
-
-//		</li>
+        const buttonRemoveElement = document.createElement('button')
+        divElement.append(buttonRemoveElement)
+        buttonRemoveElement.type = 'button'
+        buttonRemoveElement.className = 'btn btn-outline-primary'
+        buttonRemoveElement.innerText = 'Remove'
+        }
+}
 
 
 
 
-
-//js
-//----------------------------
-//methods: addEventListener,-обработчик собития
-//----------------
-//events: click, - события
-//---------------------------
-//properties: innerText, - свойства
-//-------------------------
-
-
-
-//inputElement.value = 'test test test'
-//testElement.innerText = 'another text'
-//console.log(testElement)
-
-//liElement.innerHTML =`
-//                <div class="form-group form-check">
-//                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-//                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-//                <button type="button" class="btn btn-outline-primary">Done</button>
-//                <button type="button" class="btn btn-outline-danger">Remove</button>
-//                </div>`
